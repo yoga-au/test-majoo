2e43416ef70f344d4fd947b9ae439c49474b603f
yoga-au <yogaagung.utama@gmail.com>
2021-10-30 15:04:24 +0700

    Fix: Title List, add status text to list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,25 +11,29 @@ const TodoList = () => {
     <>
       <div className="ListContainer">
         <div style={{ marginRight: "2em" }}>
-          <h3 className="ListTitle">Done</h3>
+          <h3 className="ListTitle">Not Done</h3>
           {todoNotDone.map((item) => {
             return (
               <div key={item.id} className="ListItem">
                 <h3>{item.title}</h3>
                 <p>{item.description}</p>
                 <p>{item.createdAt}</p>
+                <br />
+                <strong>{item.status ? "Done" : "Not Done"}</strong>
               </div>
             );
           })}
         </div>
         <div>
-          <h3 className="ListTitle">Not Done</h3>
+          <h3 className="ListTitle">Done</h3>
           {todoDone.map((item) => {
             return (
               <div key={item.id} className="ListItem">
                 <h3>{item.title}</h3>
                 <p>{item.description}</p>
                 <p>{item.createdAt}</p>
+                <br />
+                <strong>{item.status ? "Done" : "Not Done"}</strong>
               </div>
             );
           })}
